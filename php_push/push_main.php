36e323f6e326d7953176651e05e5e6914c5e3f7a
MAX HU <jhaoheng@gmail.com>
2016-02-28 01:42:37 +0800

    調整 code

--- php_push/push_main.php
+++ php_push/push_main.php
@@ -3,29 +3,29 @@ include ("config.php");
 include ("apns/apns.php");
 include ("gcm/gcm.php");
 
-//組合
+
 if ($category == "ios") {
-    # code...
+
     $infoArr = array(
         'apns_mode' => $apns_mode,
         'apns_phasePass' => $apns_phasePass,
         'apns_pem_path' => $apns_pem_path,
         'apns_token' => $apns_token,
         'apns_message' => $apns_message);
+
+    apns_push($infoArr);
 }
 else if ($category == "android") {
-    # code...
+    
     $infoArr = array(
         'gcm_API_KEY' => $gcm_API_KEY,
         'gcm_token' => $gcm_token,
         'gcm_message' => $gcm_message);
-}
 
-if ($category == 'ios') {
-    apns_push($infoArr);
-}
-elseif ($category == 'android') {
     gcm_push($infoArr);
+}
+else {
+    echo "error category!!";
 }
 
 //debug view
