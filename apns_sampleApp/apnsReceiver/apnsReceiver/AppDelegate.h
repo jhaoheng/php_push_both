c1aecc75844599ea26eaef272d288c321a30e607
maxhu <jhaoheng@gmail.com>
2016-03-30 00:51:42 +0800

    判斷 mail 是否設定

---
+++ apns_sampleApp/apnsReceiver/apnsReceiver/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  apnsReceiver
+//
+//  Created by max on 2016/3/30.
+//  Copyright © 2016年 max hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+{
+    ViewController *temp;
+}
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ apns_sampleApp/apnsReceiver/apnsReceiver/ViewController.h
@@ -0,0 +1,27 @@
+//
+//  ViewController.h
+//  apnsReceiver
+//
+//  Created by max on 2016/3/30.
+//  Copyright © 2016年 max hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MessageUI/MFMailComposeViewController.h>
+
+@interface ViewController : UIViewController<MFMailComposeViewControllerDelegate>
+
+@property (nonatomic, retain) NSString *passToken;
+@property (nonatomic, retain) NSDictionary *passUserInfo;
+
+@end
+
+
+
+/*
+ wanted :
+ - 需要把 logView 做得更好一些
+ - 延伸 nslog 的功能
+ */
+
+
