bd495da73f7009fd8af67cf535cc20dab0963688
max.hu <jhaoheng@gmail.com>
2016-10-23 01:59:42 +0800

    update

--- ios_apns_test/apns_http2.php
+++ ios_apns_test/apns_http2.php
@@ -1,26 +1,36 @@
 <?php
 //http://qiita.com/itosho/items/2402df4de85b360d5bd9
 
+if (!defined('CURL_HTTP_VERSION_2_0')) {
+    define('CURL_HTTP_VERSION_2_0', 3);
+}
+
 if(defined('CURL_HTTP_VERSION_2_0')){
 
-    $device_token   = '1a6869c947894a9df0a0974bf149edd4ea841fd24544ccc7d00ed7c928cb4ead';
-    $pem_file       = 'apns_pro.pem';
-    $pem_secret     = 'test';
-    $apns_topic     = 'com.idgatetest.testapp';
+    $device_token   = '837afd643ed481760874550188652b5adaa1c69fb511e4b814d9887e30f68707';
+    $pem_file       = 'apns_dev.pem';
+    $pem_secret     = '';
+    $apns_topic     = 'com.orbweb.apns';
 
 
     $sample_alert = '{"aps":{"alert":"hi","sound":"default"}}';
-    $url = "https://api.push.apple.com/3/device/$device_token";
+    $url = "https://api.development.push.apple.com/3/device/$device_token";
 
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $sample_alert);
-    curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2_0);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array("apns-topic: $apns_topic","content-length: 4096"));
-    curl_setopt($ch, CURLOPT_SSLCERT, $pem_file);
-    curl_setopt($ch, CURLOPT_SSLCERTPASSWD, $pem_secret);
+    // $ch = curl_init($url);
+    // curl_setopt($ch, CURLOPT_POSTFIELDS, $sample_alert);
+    // curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2_0);
+    // curl_setopt($ch, CURLOPT_HTTPHEADER, array("apns-topic: $apns_topic","content-length: 4096"));
+    // curl_setopt($ch, CURLOPT_SSLCERT, $pem_file);
+    // curl_setopt($ch, CURLOPT_SSLCERTPASSWD, $pem_secret);
     // curl_setopt($ch, CURLOPT_SSLVERSION, 3);
     // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,0);
     // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,0);
+    // 
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $sample_alert);
+    curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2_0);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("apns-topic: $apns_topic"));
+    curl_setopt($ch, CURLOPT_SSLCERT, $pem_file);
 
 
     $response = curl_exec($ch);
